51cb97bd9252ae7145a6045f800a36fa1f383586
TJ-Rocke <spook.devv@gmail.com>
2025-05-20 19:44:07 -0400

    Building a basic Wrapper and Reusable Component

---
+++ src/components/Input.tsx
@@ -0,0 +1,16 @@
+// prop types
+type InputProps = {
+  label: string;
+  id: string;
+};
+
+function Input({ label, id }: InputProps) {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" />
+    </p>
+  );
+}
+
+export default Input;
